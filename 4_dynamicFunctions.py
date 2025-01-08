df2c4d24edda2ca93b9338fc04b16d15c45c1de4
Nowak <vnowak@cps.k12.il.us>
2025-01-08 11:50:56 -0600

    added info

--- 4_dynamicFunctions.py
+++ 4_dynamicFunctions.py
@@ -1,3 +1,51 @@
+# def check_3_digits(number):
+#     return number in range(100, 1000)
+
+# result = check_3_digits(68)
+# print(result)
+
+# def check_3_digits(list1):
+#     for n in list1:
+#         if n in range(100, 1000):
+#             return True
+#         else:
+#             return False
+# result = check_3_digits([555, 99, 600])
+# print(result)
+
+# def check_3_digits(list1):
+
+#     three_digits_list = []
+
+#     for n in list1:
+#         if n in range(100, 1000):
+#             three_digits_list.append(n)
+#         else:
+#             pass
+
+#     return False
+
+# result = check_3_digits([555, 99, 600])
+# print(result)
+
+coffee_prices = [('cappuccino', 1.5),
+                 ('espresso', 1.2),
+                 ('mocha', 1.9)]
+
+def most_expensive_coffee(list_of_prices):
+
+    highest_price = 0
+    my_most_expensive_coffee = ''
+
+    for coffee, price in list_of_prices:
+        if price > highest_price:
+            highest_price = price
+            my_most_expensive_coffee = coffee
+        else:
+            pass
+    return(my_most_expensive_coffee, highest_price)
+print(most_expensive_coffee(coffee_prices))
+
 # Dynamic Functions Practice #1
 # Create a function (all_positives) that returns True if all the values in a list are positive, and False if at least one of the values is negative. Create a list named numbers with positive and negative values.
 
