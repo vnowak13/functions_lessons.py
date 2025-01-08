493b75d25367c429fd8925ca770f492b3777a99b
Nowak <vnowak@cps.k12.il.us>
2025-01-08 12:28:49 -0600

    added comments

--- 4_dynamicFunctions.py
+++ 4_dynamicFunctions.py
@@ -52,46 +52,46 @@
 def all_positives(list_of_positives):  # defines the list 
       #list has nothig in it now but leaves it open for numbers to be added later
     for n in list_of_positives:  #number in list
-        if n < 0:  #gives a range
-            return False
-    return True 
+        if n < 0:  #determines in n is less than 0
+            return False # if there is a negative it returns a false
+    return True #positive returns true
 
-list_of_positivies = [10, 5, 7, -3, 8]
+list_of_positivies = [10, 5, 7, -3, 8]  #list of random numbers
 # Don't call the function, you just need to define it.
-result = all_positives(list_of_positivies)
-print(result)
+result = all_positives(list_of_positivies) #give a value to make it shorter when writing the print
+print(result) #prints the function
 
 
 # Dynamic Functions Practice #2
 # Create a function (sum_less) that adds the numbers of a list as long as they are greater than 0 and less than 1000, and returns the result of said sum. Create a numbers variable, storing a list of numbers so we can test it.
-def sum_less(numbers):
-    total = 0
+def sum_less(numbers): #defining the number
+    total = 0  #starts off with zero
     for num in numbers:
         if 0 < num < 1000:  # Check if the number is between 0 and 1000
-            total + num
+            total += num   #adds them together
     return total
 
 # Test list of numbers
-numbers = [15, -10, 500, 1000]
+numbers = [15, -10, 500, 1000] #new list
 
 # Calling the function with the numbers list
-result = sum_less(numbers)
-print(result)
+result = sum_less(numbers) #puts all the info in the value "result"
+print(result) #prints the result
 
 
 
 # Dynamic Functions Practice #3
 # Create a function (count_even) that counts the number of even numbers that exist in a list (numbers), and returns the result of said count.
-def count_even(numbers):
-    count = 0
-    for num in numbers:
+def count_even(numbers): #defines the list
+    count = 0  #starting count off with zero
+    for num in numbers: #number in list
         if num % 2 == 0:  # Check if the number is even
-            count + 1
+            count + 1 #adds 1 to the count 
     return count
 
 # Test list of numbers
-numbers = [15, 7, 20, 25, 3, 1000]
+numbers = [15, 7, 20, 25, 3, 1000] #list of random numbers/ new list
 
 # Calling the function with the numbers list
-result = count_even(numbers)
-print(result)
+result = count_even(numbers) #puts all the info in the value "result"
+print(result) #prints the result/function
